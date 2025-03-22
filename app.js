be2b82462337fbfcea7b59dbb983471e3a1f44f1
mondisseous <asanch123@calstatela.edu>
2025-03-21 20:50:22 -0700

    unfinished app.js

--- app.js
+++ app.js
@@ -12,3 +12,39 @@ const printArtist = (artist) => {
     console.log(`Spotify URL: ${artist.url}`);
     console.log('----------------------------');
 };
+
+// main search function
+export const searchArtistByKeyboard = async (keyword) => {
+    try {
+        const data = await api.searchAPI(keyword);
+
+        if (!data.items || data.items.length === 0) {
+            console.log('No artists found');
+            return;
+        }
+
+        const results = data.items;
+
+        await db.insert('search_history_keyword', {keyword});
+
+        const { selectedID } = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'selectedID',
+                message: 'Select an artist',
+                choices: results.map((artist) => ({
+                    name: `${artist.name} (${artist.genres?.join(', ') || 'Unknown'})`,
+                    value: artist.id,
+                })),
+            },
+        ]);
+        // unfinished
+        const selectedArtist = results.find((artist) => artist.id === selectedID);
+
+        await db.insert('search_history_artist', selectedArtist);
+        printArtist(selectedArtist);
+        
+    } catch (error) {
+        console.error('Search Error:', error.message);
+    }
+};
