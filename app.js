f764c0c2ca0115c72e84a48c2b13d6af61089145
mondisseous <asanch123@calstatela.edu>
2025-03-21 19:22:41 -0700

    helper function -  app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 import inquirer from 'inquirer';
 import * as api from './api.js';
-import * as api from './db.js';
+import * as db from './db.js';
+
+// display helper
+const printArtist = (artist) => {
+    console.log('\n Artist Info:');
+    console.log(`Name: ${artist.name}`);
+    console.log(`Genre: ${artist.genre}`);
+    console.log(`Popularity: ${artist.popularity}`);
+    console.log(`Followers: ${artist.followers}`);
+    console.log(`Spotify URL: ${artist.url}`);
+    console.log('----------------------------');
+};
