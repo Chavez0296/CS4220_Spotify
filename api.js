ce6c99f401ccd80212a90d70be6ab2cc5a90b0a5
UndeadGoonie <98503477+UndeadGoonie@users.noreply.github.com>
2025-03-26 20:40:43 -0700

    Update api.js

--- api.js
+++ api.js
@@ -4,13 +4,12 @@ import querystring from 'querystring';
 
 dotenv.config();
 
-
 const clientID = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const tokenUrl = 'https://accounts.spotify.com/api/token'
+const tokenUrl = 'https://accounts.spotify.com/api/token';
 
-export const getToken = async() =>{
-    try{
+export const getToken = async () => {
+    try {
         const response = await axios.post(tokenUrl,
             querystring.stringify({ grant_type: 'client_credentials' }), // Properly encode body
             {
@@ -22,29 +21,46 @@ export const getToken = async() =>{
         );
     
         return response.data.access_token;
-    }catch (error) {
+    } catch (error) {
         console.error('Error fetching token:', error.response?.data || error.message);
         throw error;
     }
 };
 
-export const searchAPI = async(searchName) =>{
-    try{
-    const token = await getToken();
+export const searchAPI = async (searchName) => {
+    try {
+        const token = await getToken();
 
-    const endpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchName)}&type=artist&limit=1`;
-    const response = await axios.get(endpoint, {
-        headers: { Authorization: `Bearer ${token}` 
-        }
-    });
+        const endpoint = `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchName)}&type=artist&limit=1`;
+        const response = await axios.get(endpoint, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
 
-    return response.data.artists;
-
-    }catch(error){
+        return response.data.artists;
+    } catch (error) {
         console.log('Error fetching artist', error.response?.data || error.message);
         throw error;
     }
-}
+};
+
+export const searchID = async (id) => {
+    try {
+        const token = await getToken();
+
+        const endpoint = `https://api.spotify.com/v1/artists/${encodeURIComponent(id)}`;
+        const response = await axios.get(endpoint, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log('Error fetching id', error.response?.data || error.message);
+        throw error;
+    }
+};
 
 const art = await searchAPI('Eminem');
 console.log(art);
+
+const testing = await searchID('0C0XlULifJtAgn6ZNCW2eu');
+console.log(testing);
